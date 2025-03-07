d5f4bf27c6d250a18e7ca0a843ab0ebe5ab62849
Abby Ressner <abbyressner@gmail.com>
2025-03-06 20:38:51 -0600

    implement Hash class

--- src/main/java/edu/grinnell/csc207/blockchain/Hash.java
+++ src/main/java/edu/grinnell/csc207/blockchain/Hash.java
@@ -1,8 +1,70 @@
 package edu.grinnell.csc207.blockchain;
 
-/**
- * A wrapper class over a hash value (a byte array).
- */
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Hash {
-    // TODO: fill me in!
+
+    private byte[] data;
+
+    /**
+     * Constructs a new Hash object.
+     *
+     * @param data the byte array representing the hash data
+     */
+    public Hash(byte[] data) {
+        this.data = data;
+    }
+
+    /**
+     * Returns the byte array of the hash data.
+     *
+     * @return the byte array of the hash data
+     */
+    public byte[] getData() {
+        return this.data;
+    }
+
+    /**
+     * Returns whether the hash is valid.
+     *
+     * @return the resulting value of boolean `valid`
+     */
+    public boolean isValid() {
+        boolean valid = false;
+        if (data[0] == 0 && data[1] == 0 && data[2] == 0) {
+            valid = true;
+        }
+        return valid;
+    }
+
+    /**
+     * Returns the string representation of the hash as a string of hexadecimal
+     * digits (2 digits per byte).
+     *
+     * @return the string representation of the hash
+     */
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : data) {
+            sb.append(String.format("%02x", Byte.toUnsignedInt(b)));
+        }
+        return sb.toString();
+    }
+
+    
+
+    public boolean equals(Object other) {
+        if (other instanceof Hash) {
+            Hash otherHash = (Hash) other;
+            return this.toString().equals(otherHash.toString());
+        }
+        return false;
+    }
+
+    public static byte[] calculateHash(String msg) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("sha-256");
+
+    }
+
 }
