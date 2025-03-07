a19920fce3035d1d2995d601556fbfafa6b14f0d
Abby Ressner <abbyressner@gmail.com>
2025-03-06 22:34:34 -0600

    implement Block class

--- src/main/java/edu/grinnell/csc207/blockchain/Block.java
+++ src/main/java/edu/grinnell/csc207/blockchain/Block.java
@@ -1,8 +1,89 @@
 package edu.grinnell.csc207.blockchain;
 
-/**
- * A single block of a blockchain.
- */
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Block {
-    // TODO: fill me in!
+
+    private int num;
+    private int amount;
+    private Hash prevHash;
+    private long nonce;
+    private Hash hash;
+
+    public Block(int num, int amount, Hash prevHash) throws NoSuchAlgorithmException {
+        this.num = num;
+        this.amount = amount;
+        this.prevHash = prevHash;
+        this.nonce = miner();
+        this.hash = calculateHash();
+    }
+
+    public Block(int num, int amount, Hash prevHash, long nonce) throws NoSuchAlgorithmException {
+        this.num = num;
+        this.amount = amount;
+        this.prevHash = prevHash;
+        this.nonce = nonce;
+        this.hash = calculateHash();
+    }
+
+    public long miner() throws NoSuchAlgorithmException {
+        long calculatedNonce = 0;
+        Hash newHash = calculateNoNonceHash(calculatedNonce);
+        while (!newHash.isValid()) {
+            calculatedNonce++;
+            newHash = calculateNoNonceHash(calculatedNonce);
+        }
+        return calculatedNonce;
+    }
+
+    public Hash calculateHash() throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("sha-256");
+        ByteBuffer b = ByteBuffer.allocate(64).putInt(this.num).putInt(this.amount).putLong(this.nonce);
+        if (this.prevHash != null) {
+            b.put(this.prevHash.getData());
+        }
+        md.update(b.array());
+        byte[] hashBytes = md.digest();
+        return new Hash(hashBytes);
+    }
+
+    public Hash calculateNoNonceHash(long nonce) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("sha-256");
+        ByteBuffer b = ByteBuffer.allocate(64).putInt(this.num).putInt(this.amount).putLong(nonce);
+        if (this.prevHash != null) {
+            b.put(this.prevHash.getData());
+        }
+        md.update(b.array());
+        byte[] hashBytes = md.digest();
+        return new Hash(hashBytes);
+    }
+
+    public int getNum() {
+        return this.num;
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+
+    public long getNonce() {
+        return this.nonce;
+    }
+
+    public Hash getHash() {
+        return this.hash;
+    }
+
+    public Hash getPrevHash() {
+        return this.prevHash;
+    }
+
+    @Override
+    public String toString() {
+        return "Block " + this.num + " (Amount: " + this.amount + ", Nonce: " + this.nonce
+                + ", prevHash: " + (this.prevHash == null ? "null" : this.prevHash)
+                + ", hash: " + this.hash + ")";
+    }
 }
